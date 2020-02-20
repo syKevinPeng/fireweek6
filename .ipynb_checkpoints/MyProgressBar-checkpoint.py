c337c9b1063250a11266d2674e818d0fc25c0a53
Tuge <peng2000@terpmail.umd.edu>
2020-02-19 20:16:07 -0500

    finish positive image arr in data generator

---
+++ .ipynb_checkpoints/MyProgressBar-checkpoint.py
@@ -0,0 +1,16 @@
+# Optional, download progress bar
+class MyProgressBar():
+    def __init__(self):
+        self.pbar = None
+        self.bar.star()
+
+    def __call__(self, block_num, block_size, total_size):
+        if not self.pbar:
+            self.pbar=progressbar.ProgressBar(maxval=total_size)
+            self.pbar.start()
+
+        downloaded = block_num * block_size
+        if downloaded < total_size:
+            self.pbar.update(downloaded)
+        else:
+            self.pbar.finish()
